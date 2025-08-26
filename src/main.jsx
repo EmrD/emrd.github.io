7928ea2acd6a7ba7aa550634f156ed7edfaaf776
Emr <127445447+EmrD@users.noreply.github.com>
2025-08-26 16:56:36 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,12 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
-    <App />
+    <BrowserRouter>
+    <Routes>
+        <Route path="*" element={<App />} />
+    </Routes>
+    </BrowserRouter>
 )
