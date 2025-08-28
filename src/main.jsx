d3a8a6fbdb99e3a87b852ea2f227b9a25258fe49
emrd434 <emrd434@gmail.com>
2025-08-28 20:20:20 +0000

    Production commit

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,11 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Projects from './Projects.jsx'
 
 createRoot(document.getElementById('root')).render(
     <BrowserRouter>
     <Routes>
         <Route path="/" element={<App />} />
-        <Route path="/all-projects" element={<Projects />} />
         <Route path="*" element={<center><h1>Page Not Found!</h1></center>} />
     </Routes>
     </BrowserRouter>
